19254441806018cdc84200c475db3f32978e21f7
victory <wagorvictory@gmail.com>
2023-03-28 22:22:45 +0100

    step-13

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -2,3 +2,4 @@ const loginRoute = '/login/';
 const registerRoute = '/register/';
 const notesRoute = '/notes/';
 const verifyEmailRoute = '/verrify-email/';
+const newNoteRoute = '/notes/new-note/';
